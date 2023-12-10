c867ec53a9c09fd61f3f717b01624a34ffa95a68
mariooo1234 <1thermaltake2705@gmail.com>
2023-12-11 00:54:11 +0300

    Task. Implementing the Cool Timer behavior

--- app/src/main/java/com/example/saving/MainActivity.java
+++ app/src/main/java/com/example/saving/MainActivity.java
@@ -5,43 +5,113 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private SeekBar seekBar;
+    private TextView textView;
+    private Button button;
+    private CountDownTimer countDownTimer;
+    private boolean isTimerOn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CountDownTimer myTimer = new CountDownTimer(10000, 1000) {
+        seekBar = findViewById(R.id.seekBar);
+        textView = findViewById(R.id.textView);
+
+        seekBar.setMax(600);
+        seekBar.setProgress(30);
+
+        isTimerOn = false;
+
+        button = findViewById(R.id.button);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onTick(long millisUntilFinished) {
-                Log.d("myTimer: ", String.valueOf(millisUntilFinished / 1000) + " second left.");
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                long progressInMillis = progress * 1000;
+                updateTimer(progressInMillis);
             }
 
             @Override
-            public void onFinish() {
-                Log.d("myTimer: ", "Finish!()");
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
             }
-        };
 
-        myTimer.start();
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
 
-//        final Handler handler = new Handler();
-//
-//        Runnable runnable = new Runnable() {
-//            @Override
-//            public void run() {
-//                Log.d("Runnable: ", "Two seconds are passed");
-//                handler.postDelayed(this, 2000);
-//            }
-//        };
-//
-//        handler.post(runnable);
+            }
+        });
+    }
+
+    public void start(View view) {
+        if (!isTimerOn) {
+            button.setText("Stop");
+            seekBar.setEnabled(false);
+            isTimerOn = true;
+
+            countDownTimer = new CountDownTimer(seekBar.getProgress() * 1000, 1000) {
+
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    updateTimer(millisUntilFinished);
+                }
+
+                @Override
+                public void onFinish() {
+                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.bell_sound);
+                    mediaPlayer.start();
+                    resetTimer();
+                }
+            };
+
+            countDownTimer.start();
+        } else {
+            resetTimer();
+        }
+    }
+
+    private void updateTimer (long millisUntilFinished) {
+        int minutes = (int) millisUntilFinished / 1000 / 60;
+        int seconds = (int) millisUntilFinished / 1000 - (minutes * 60);
+
+        String minutesStr = "";
+        String secondsStr = "";
+
+        if (minutes < 10) {
+            minutesStr = "0" + minutes;
+        } else {
+            minutesStr = String.valueOf(minutes);
+        }
+
+        if (seconds < 10) {
+            secondsStr = "0" + seconds;
+        } else {
+            secondsStr = String.valueOf(seconds);
+        }
+
+        textView.setText(minutesStr + ":" + secondsStr);
+    }
+
+    private void resetTimer() {
+        countDownTimer.cancel();
+        textView.setText("00:30");
+        button.setText("Start");
+        seekBar.setEnabled(true);
+        seekBar.setProgress(30);
+        isTimerOn = false;
     }
 }
