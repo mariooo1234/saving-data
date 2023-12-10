2f1c34746fbde9b7c6edf1fe727d55e2a05a5e16
mariooo1234 <1thermaltake2705@gmail.com>
2023-12-11 01:07:51 +0300

    Task. Implementation of Cool Timer behavior. Part 2

--- app/src/main/java/com/example/saving/MainActivity.java
+++ app/src/main/java/com/example/saving/MainActivity.java
@@ -10,6 +10,7 @@ import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.PersistableBundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -31,25 +32,8 @@ public class MainActivity extends AppCompatActivity {
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                int minutes = progress / 60;
-                int seconds = progress - (minutes * 60);
-
-                String minutesStr = "";
-                String secondsStr = "";
-
-                if (minutes < 10) {
-                    minutesStr = "0" + minutes;
-                } else {
-                    minutesStr = String.valueOf(minutes);
-                }
-
-                if (seconds < 10) {
-                    secondsStr = "0" + seconds;
-                } else {
-                    secondsStr = String.valueOf(seconds);
-                }
-
-                textView.setText(minutesStr + ":" + secondsStr);
+                long progressInMillis = progress * 1000;
+                updateTimer(progressInMillis);
             }
 
             @Override
@@ -62,5 +46,42 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void start(View view) {
+        new CountDownTimer(seekBar.getProgress() * 1000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                updateTimer(millisUntilFinished);
+            }
+
+            @Override
+            public void onFinish() {
+                Log.d("onFinish: ", "Finish!");
+            }
+        }.start();
+    }
+
+    private void updateTimer (long millisUntilFinished) {
+        int minutes = (int) millisUntilFinished / 1000 / 60;
+        int seconds = (int) millisUntilFinished / 1000 - (minutes * 60);
+
+        String minutesStr = "";
+        String secondsStr = "";
+
+        if (minutes < 10) {
+            minutesStr = "0" + minutes;
+        } else {
+            minutesStr = String.valueOf(minutes);
+        }
+
+        if (seconds < 10) {
+            secondsStr = "0" + seconds;
+        } else {
+            secondsStr = String.valueOf(seconds);
+        }
+
+        textView.setText(minutesStr + ":" + secondsStr);
     }
 }
