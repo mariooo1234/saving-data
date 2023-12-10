142e7114ee2edd60ce05c6433079940d1dbcff81
mariooo1234 <1thermaltake2705@gmail.com>
2023-12-11 01:14:21 +0300

    Task. Implementation of Cool Timer behavior. Part 3

--- app/src/main/java/com/example/saving/MainActivity.java
+++ app/src/main/java/com/example/saving/MainActivity.java
@@ -5,18 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private SeekBar seekBar;
     private TextView textView;
+    private Button button;
+    private CountDownTimer countDownTimer;
+    private boolean isTimerOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +32,11 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.textView);
 
         seekBar.setMax(600);
-        seekBar.setProgress(60);
+        seekBar.setProgress(30);
+
+        isTimerOn = false;
+
+        button = findViewById(R.id.button);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -49,18 +58,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void start(View view) {
-        new CountDownTimer(seekBar.getProgress() * 1000, 1000) {
+        if (!isTimerOn) {
+            button.setText("Stop");
+            seekBar.setEnabled(false);
+            isTimerOn = true;
 
-            @Override
-            public void onTick(long millisUntilFinished) {
-                updateTimer(millisUntilFinished);
-            }
+            countDownTimer = new CountDownTimer(seekBar.getProgress() * 1000, 1000) {
 
-            @Override
-            public void onFinish() {
-                Log.d("onFinish: ", "Finish!");
-            }
-        }.start();
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    updateTimer(millisUntilFinished);
+                }
+
+                @Override
+                public void onFinish() {
+                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.bell_sound);
+                    mediaPlayer.start();
+                    resetTimer();
+                }
+            };
+
+            countDownTimer.start();
+        } else {
+            resetTimer();
+        }
     }
 
     private void updateTimer (long millisUntilFinished) {
@@ -83,5 +104,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         textView.setText(minutesStr + ":" + secondsStr);
+    }
+
+    private void resetTimer() {
+        countDownTimer.cancel();
+        textView.setText("00:30");
+        button.setText("Start");
+        seekBar.setEnabled(true);
+        seekBar.setProgress(30);
+        isTimerOn = false;
     }
 }
