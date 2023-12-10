393d5b0289b15a0789f9df60146b7abb27a66f58
mariooo1234 <1thermaltake2705@gmail.com>
2023-12-11 01:01:55 +0300

    Task. Implementation of Cool Timer behavior. Part 1

--- app/src/main/java/com/example/saving/MainActivity.java
+++ app/src/main/java/com/example/saving/MainActivity.java
@@ -5,23 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
 import android.os.PersistableBundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private SeekBar seekBar;
     private TextView textView;
-    private Button button;
-    private CountDownTimer countDownTimer;
-    private boolean isTimerOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,17 +26,30 @@ public class MainActivity extends AppCompatActivity {
         textView = findViewById(R.id.textView);
 
         seekBar.setMax(600);
-        seekBar.setProgress(30);
-
-        isTimerOn = false;
-
-        button = findViewById(R.id.button);
+        seekBar.setProgress(60);
 
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                long progressInMillis = progress * 1000;
-                updateTimer(progressInMillis);
+                int minutes = progress / 60;
+                int seconds = progress - (minutes * 60);
+
+                String minutesStr = "";
+                String secondsStr = "";
+
+                if (minutes < 10) {
+                    minutesStr = "0" + minutes;
+                } else {
+                    minutesStr = String.valueOf(minutes);
+                }
+
+                if (seconds < 10) {
+                    secondsStr = "0" + seconds;
+                } else {
+                    secondsStr = String.valueOf(seconds);
+                }
+
+                textView.setText(minutesStr + ":" + secondsStr);
             }
 
             @Override
@@ -55,63 +62,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-    }
-
-    public void start(View view) {
-        if (!isTimerOn) {
-            button.setText("Stop");
-            seekBar.setEnabled(false);
-            isTimerOn = true;
-
-            countDownTimer = new CountDownTimer(seekBar.getProgress() * 1000, 1000) {
-
-                @Override
-                public void onTick(long millisUntilFinished) {
-                    updateTimer(millisUntilFinished);
-                }
-
-                @Override
-                public void onFinish() {
-                    MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.bell_sound);
-                    mediaPlayer.start();
-                    resetTimer();
-                }
-            };
-
-            countDownTimer.start();
-        } else {
-            resetTimer();
-        }
-    }
-
-    private void updateTimer (long millisUntilFinished) {
-        int minutes = (int) millisUntilFinished / 1000 / 60;
-        int seconds = (int) millisUntilFinished / 1000 - (minutes * 60);
-
-        String minutesStr = "";
-        String secondsStr = "";
-
-        if (minutes < 10) {
-            minutesStr = "0" + minutes;
-        } else {
-            minutesStr = String.valueOf(minutes);
-        }
-
-        if (seconds < 10) {
-            secondsStr = "0" + seconds;
-        } else {
-            secondsStr = String.valueOf(seconds);
-        }
-
-        textView.setText(minutesStr + ":" + secondsStr);
-    }
-
-    private void resetTimer() {
-        countDownTimer.cancel();
-        textView.setText("00:30");
-        button.setText("Start");
-        seekBar.setEnabled(true);
-        seekBar.setProgress(30);
-        isTimerOn = false;
     }
 }
