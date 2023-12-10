d81659ef867e822fadcbbb6c932a0cb8fe846578
mariooo1234 <1thermaltake2705@gmail.com>
2023-12-11 00:49:15 +0300

    Task. Cool Timer Markup

--- app/src/main/java/com/example/saving/MainActivity.java
+++ app/src/main/java/com/example/saving/MainActivity.java
@@ -1,63 +1,47 @@
 package com.example.saving;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView textView;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = findViewById(R.id.textView);
+        CountDownTimer myTimer = new CountDownTimer(10000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                Log.d("myTimer: ", String.valueOf(millisUntilFinished / 1000) + " second left.");
+            }
 
-        Log.d("LifeCycle method: ", "onCreate!");
-        textView.append("onCreate!" + "\n");
-    }
+            @Override
+            public void onFinish() {
+                Log.d("myTimer: ", "Finish!()");
+            }
+        };
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+        myTimer.start();
 
-        Log.d("LifeCycle method: ", "onStart!");
-        textView.append("onStart!" + "\n");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        Log.d("LifeCycle method: ", "onResume!");
-        textView.append("onResume!" + "\n");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        Log.d("LifeCycle method: ", "onPause!");
-        textView.append("onPause!" + "\n");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        Log.d("LifeCycle method: ", "onStop!");
-        textView.append("onStop!" + "\n");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        Log.d("LifeCycle method: ", "onDestroy!");
-        textView.append("onDestroy!" + "\n");
+//        final Handler handler = new Handler();
+//
+//        Runnable runnable = new Runnable() {
+//            @Override
+//            public void run() {
+//                Log.d("Runnable: ", "Two seconds are passed");
+//                handler.postDelayed(this, 2000);
+//            }
+//        };
+//
+//        handler.post(runnable);
     }
 }
